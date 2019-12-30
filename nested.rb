d1f4d780a134e0c564c2a248c34c8432ba3a6eb3
Will Carter <will.carter@fergusllc.com>
2019-12-30 00:38:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,6 +87,8 @@ def adding_matz
 end
 
 def changing_alan
+	#change what Alan Kay is :known_for to the value of the alans_new_info variable.
+	alans_new_info = "GUI"
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -102,11 +104,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-		#  programmer_hash[:alan_kay][:known_for] = alans_new_info
-
-
+     
 end
 
 def adding_to_dennis
