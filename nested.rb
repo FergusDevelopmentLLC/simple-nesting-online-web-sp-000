921a64ec7bc43ffec66203eb3966f6f85b46e32f
Will Carter <will.carter@fergusllc.com>
2019-12-30 00:36:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,6 @@ def adding_matz
         }
      }
 end
-puts adding_matz
 
 def changing_alan
 	programmer_hash =
@@ -105,6 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 
 end
