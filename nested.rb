ef696f1e714b772cdf4996c50c4582bbcf53543b
Will Carter <will.carter@fergusllc.com>
2019-12-30 00:26:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,12 +84,12 @@ def adding_matz
 		 #   :known_for => "Ruby",
 		 #    :languages => ["LISP", "C"]
 		 # }
-		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto] = [:known_for]
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 		 programmer_hash[:yukihiro_matsumoto]
 end
-puts adding_matz
+puts adding_matz[programmer_hash]
 
 def changing_alan
 	programmer_hash =
